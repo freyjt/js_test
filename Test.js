ddd381f99adb1573cc1ef433aa4d632b1c781669
Joshua Frey <freyjt@gmail.com>
2015-12-14 00:55:40 -0500

    Added a report generator method.

--- Test.js
+++ Test.js
@@ -36,3 +36,24 @@ Test.prototype.assertEqualish = function( thingOne, thingTwo, descriptor ) {
     }
 
 }
+
+Test.prototype.getReport = function( ) {
+    if(this.failedTests.length >= 0) {
+        var i;
+        for(i = 0; i < this.failedTests.length; i++) {
+            console.log("  **************************************************");
+            console.log("   Test Failed:");
+            console.log("     " + this.failedTests[i]);
+            console.log("  **************************************************");
+        }
+    }
+    var totalTests = this.passedTests.length + this.failedTests.length;
+    var pPassed    = (this.passedTests.length * 100 / totalTests);
+    console.log("  **************************************************");
+    console.log("  Conducted " + totalTests + " tests.");
+    console.log("    Passed " + this.passedTests.length + " tests.");
+    console.log("     " + pPassed + " Percent passed");
+    var status == (pPassed == 1) ? "green" : "red";
+    console.log("    status: " + status);
+    console.log("  **************************************************");
+}
