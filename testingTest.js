1631d70befc20307c71385deb8cdf894aa82155e
Joshua Frey <freyjt@gmail.com>
2015-12-14 01:59:24 -0500

    Continued writing test script. Working well.

--- testingTest.js
+++ testingTest.js
@@ -4,6 +4,12 @@
 function letsCallItMain( ) {
 
     var Test = require('./Test.js')['Test'];
+    var test = new Test();
 
+    var three = 3;
+    var ohter = '3';
 
+    test.assertEqual(three, three, "Asserting same object three is equal.");
+
+    test.getReport();
 } letsCallItMain();8
