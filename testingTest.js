e14479238d7fe75132884f473a3b178245e3c490
Joshua Frey <freyjt@gmail.com>
2015-12-18 16:29:56 -0500

    Wrote tests for assertType. Green.

--- testingTest.js
+++ testingTest.js
@@ -16,7 +16,10 @@ function letsCallItMain( ) {
     test.assertNotEqual(three, three, "Asserting same object not Equal. WANT FAILURE.");
     test.assertNotEquallish(three, two, "Asserting different object representations are not equallish");
     test.assertNotEquallish(three, other, "Asserting different object representations of\n same number are not equallish. WANT FAILURE.");
+    test.assertType('string', other, "Asserting other is of type string.");
+    test.assertType('number', other, "Asserting string, other, is of type number. WANT FAILURE");
 
+    
     test.getReport();
 
 } letsCallItMain();
